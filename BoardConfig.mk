e0ed71128f338861f47117de5653a14739005878
lllgts <wuer031@126.com>
2022-01-01 15:33:07 +0000

    joan:Fix build error&add prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,6 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Init
