cd2611e769531dfd2998fa9ab6744d070b11b209
lllgts <wuer031@126.com>
2022-01-02 09:36:54 +0000

    exthm ify

---
+++ exthm_joan.mk
@@ -0,0 +1,35 @@
+# Inherit some common exthm stuff.
+$(call inherit-product, vendor/exthm/config/common.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from joan device
+$(call inherit-product, device/lge/joan/device.mk)
+
+# Device identifiers
+PRODUCT_DEVICE := joan
+PRODUCT_NAME := exthm_joan
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := joan
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RELEASE_NAME := V30
+
+PRODUCT_GMS_CLIENTID_BASE := android-om-lg
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="joan" \
+    PRODUCT_DEVICE="joan" \
+    PRODUCT_NAME="joan_global_com" \
+    PRIVATE_BUILD_DESC="joan_global_com-user 9 PKQ1.190414.001 192451212666b release-keys"
+
+BUILD_FINGERPRINT="lge/joan_global_com/joan:9/PKQ1.190414.001/192451212666b:user/release-keys"
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model ro.product.system.model
