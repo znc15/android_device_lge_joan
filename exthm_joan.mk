a5a61689c3b086fbc066354884ce840e54aef61c
lllgts <76236362+lllgts@users.noreply.github.com>
2022-01-17 16:33:13 +0800

    joan:Enable updating of APEXes

--- exthm_joan.mk
+++ exthm_joan.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from joan device
 $(call inherit-product, device/lge/joan/device.mk)
 
