ffc9a03306ffd31aaed5b3f5a947e97a982bbc52
lllgts <wuer031@126.com>
2022-01-18 12:37:23 +0000

    Revert "joan: add USB Gadget HAL"
    This reverts commit b442dd523e78d142bf2709df953856531a802b08.

--- device.mk
+++ device.mk
@@ -467,8 +467,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
-    android.hardware.usb.gadget@1.0
+    android.hardware.usb@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
