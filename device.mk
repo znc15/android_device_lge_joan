f940ccab2ab35a1fe31ccdb29a2d39e5ff927bbd
lllgts <wuer031@126.com>
2022-02-02 11:18:27 +0800

    Revert "joan-common: switch back to HMP"
    This reverts commit c63522597ed4101b8e9475879862426d2938193d.

--- device.mk
+++ device.mk
@@ -373,10 +373,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.2-service.joan-libperfmgr \
+    libxml2
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QCOM
 PRODUCT_COPY_FILES += \
