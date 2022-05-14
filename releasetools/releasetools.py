3d8692c293ed4acc69933f29e1615be1a698cc70
lllgts <wuer031@126.com>
2022-05-14 00:49:44 +0000

    Revert "[WIP]joan: remove lock screen password in flash complete"
    This reverts commit a1592253c0145975e1484484276ed4dba6d61457.

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -15,5 +15,5 @@
 #
 
 def FullOTA_InstallEnd(info):
-    info.script.AppendExtra('delete("/data/system/locksettings.db");')
+    info.script.AppendExtra('assert(run_program("/tmp/install/bin/device_check.sh") == 0);');
 
